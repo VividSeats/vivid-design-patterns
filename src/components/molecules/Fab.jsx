cf8f045ede69d5a3c673bdb9d6a270b67400f90a
Paige Vogenthaler <paige.vogenthaler@vividseats.com>
2019-01-21 14:12:46 -0600

    WEB-5744 add data-state to fab component

--- src/components/molecules/Fab.jsx
+++ src/components/molecules/Fab.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import FabChild from '../atoms/FabChild';
 
-const Fab = ({ children, className = '', ...htmlAttributes }) => (
-    <div className={`vp-fab ${className}`} {...htmlAttributes}>
+const Fab = ({ children, className = '', dataState = 'visible', ...htmlAttributes }) => (
+    <div className={`vp-fab ${className}`} {...htmlAttributes} data-state={dataState}>
         {React.Children.toArray(children).map(child => {
             const { type } = child;
             if (!type || type.displayName === 'FabChild') {
@@ -24,7 +24,8 @@ Fab.Child = FabChild;
 
 Fab.propTypes = {
     children: PropTypes.node,
-    className: PropTypes.string
+    className: PropTypes.string,
+    dataState: PropTypes.oneOf(['visible', 'hidden'])
 };
 
 export default Fab;
