e99feaa70782662549b9e6cfd1bdddbf2999a84a
Paige Vogenthaler <paige.vogenthaler@vividseats.com>
2019-01-21 14:31:04 -0600

    WEB-5744 update fab unit tests

--- src/components/molecules/Fab.jsx
+++ src/components/molecules/Fab.jsx
@@ -2,30 +2,41 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import FabChild from '../atoms/FabChild';
 
-const Fab = ({ children, className = '', dataState = 'visible', ...htmlAttributes }) => (
-    <div className={`vp-fab ${className}`} {...htmlAttributes} data-state={dataState}>
-        {React.Children.toArray(children).map(child => {
-            const { type } = child;
-            if (!type || type.displayName === 'FabChild') {
-                return child;
-            }
+const Fab = ({ children, className = '', dataState = Fab.DATA_STATE.VISIBLE, ...htmlAttributes }) => {
+    if (dataState === Fab.DATA_STATE.HIDDEN) {
+        return null;
+    }
 
-            // wrap child in sub-component if not already wrapped
-            return (
-                <Fab.Child key={`${!!type ? type : 'FabChild'}-${new Date().getTime()}`} {...htmlAttributes}>
-                    {child}
-                </Fab.Child>
-            );
-        })}
-    </div>
-);
+    return (
+        <div className={`vp-fab ${className}`} {...htmlAttributes} data-state={dataState}>
+            {React.Children.toArray(children).map(child => {
+                const { type } = child;
+                if (!type || type.displayName === 'FabChild') {
+                    return child;
+                }
+
+                // wrap child in sub-component if not already wrapped
+                return (
+                    <Fab.Child key={`${!!type ? type : 'FabChild'}-${new Date().getTime()}`} {...htmlAttributes}>
+                        {child}
+                    </Fab.Child>
+                );
+            })}
+        </div>
+    );
+};
+
+Fab.DATA_STATE = {
+    VISIBLE: 'visible',
+    HIDDEN: 'hidden'
+};
 
 Fab.Child = FabChild;
 
 Fab.propTypes = {
     children: PropTypes.node,
     className: PropTypes.string,
-    dataState: PropTypes.oneOf(['visible', 'hidden'])
+    dataState: PropTypes.oneOf([Fab.DATA_STATE.VISIBLE, Fab.DATA_STATE.HIDDEN])
 };
 
 export default Fab;
--- test/Fab/FabSpec.js
+++ test/Fab/FabSpec.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
-import Fab from '../../src/components/atoms/Fab';
+import Fab from '../../src/components/molecules/Fab';
 
 /* eslint react/jsx-filename-extension: 0 */
 
@@ -8,5 +8,32 @@ describe('<Fab />', () => {
     it('renders', () => {
         const wrapper = shallow(<Fab />);
         expect(wrapper.hasClass('vp-fab')).toBe(true);
+    });
+
+    it('does not render a component with dataState hidden prop', () => {
+        const wrapper = mount(<Fab dataState="hidden" />);
+        expect(wrapper.exists('.vp-fab')).toBe(false);
+    });
+
+    it('supports FabChild subcomponents', () => {
+        const wrapper = mount(
+            <Fab>
+                <Fab.Child className="child1">Child1</Fab.Child>
+                <Fab.Child className="child2">Child2</Fab.Child>
+            </Fab>
+        );
+
+        expect(wrapper.exists('.vp-fab__child.child1'));
+        expect(wrapper.exists('.vp-fab__child.child2'));
+    });
+
+    it('wraps a FabChild subcomponent around children that are not subcomponents', () => {
+        const wrapper = mount(
+            <Fab>
+                <span>I am a child</span>
+            </Fab>
+        );
+
+        expect(wrapper.exists('.vp-fab__child span'));
     });
 });
