15a09ab4c3cf63f9dd3ceadf5fe5b50c76c61412
Paige Vogenthaler <paige.vogenthaler@vividseats.com>
2019-01-21 13:51:35 -0600

    WEB-5744 style and add fab componet and docs

--- src/components/molecules/Fab.jsx
+++ src/components/molecules/Fab.jsx
@@ -1,17 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import FabChild from '../atoms/FabChild';
 
-const Fab = ({
-    children,
-    ...htmlAttributes
-}) => (
-    <div className='vp-fab' { ...htmlAttributes } >
-        { children }
+const Fab = ({ children, className = '', ...htmlAttributes }) => (
+    <div className={`vp-fab ${className}`} {...htmlAttributes}>
+        {React.Children.toArray(children).map(child => {
+            const { type } = child;
+            if (!type || type.displayName === 'FabChild') {
+                return child;
+            }
+
+            // wrap child in sub-component if not already wrapped
+            return (
+                <Fab.Child key={`${!!type ? type : 'FabChild'}-${new Date().getTime()}`} {...htmlAttributes}>
+                    {child}
+                </Fab.Child>
+            );
+        })}
     </div>
 );
 
+Fab.Child = FabChild;
+
 Fab.propTypes = {
-    children: PropTypes.node.isRequired
+    children: PropTypes.node,
+    className: PropTypes.string
 };
 
 export default Fab;
