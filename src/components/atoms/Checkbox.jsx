9d27cb483ddbd2c6bdfe284444f59d7b6aaa0de1
Yucheng Lin <yuchenglin3@gmail.com>
2019-01-31 15:31:17 -0600

    add unit tests

--- src/components/atoms/Checkbox.jsx
+++ src/components/atoms/Checkbox.jsx
@@ -2,32 +2,57 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class Checkbox extends React.Component {
-    state = {
-        checked: this.props.defaultChecked
+    state = { checked: this.props.defaultChecked, error: '' };
+
+    getCheckedState = () => {
+        if (this.isControlled()) {
+            return this.props.checked;
+        }
+
+        return this.state.checked;
     };
 
-    onChange = () => {
-        this.setState(
-            ({ checked }) => ({ checked: !checked }),
-            () => {
-                this.props.onChange(this.state.checked);
-            }
-        );
+    onClick = () => {
+        if (this.isControlled()) {
+            const error = this.props.validationMethod(!this.getCheckedState());
+            this.setState({
+                error
+            });
+            this.props.onChange(!this.getCheckedState());
+        } else {
+            this.setState(
+                ({ checked }) => ({ checked: !checked }),
+                () => {
+                    const { state } = this;
+                    const error = this.props.validationMethod(state.checked);
+                    this.setState({
+                        error
+                    });
+                    this.props.onChange(state.checked);
+                }
+            );
+        }
     };
+
+    isControlled() {
+        return typeof this.props.checked !== 'undefined';
+    }
 
     render() {
         const { className, onClick, validationMethod, defaultChecked, label, id, ...htmlAttributes } = this.props;
         return (
-            <label className={`${className} vp-checkbox`} aria-label="checkbox" htmlFor={id}>
+            <label className={`${className} vp-control--checkbox`} aria-label="checkbox" htmlFor={id}>
                 <input
+                    hidden
                     type="checkbox"
-                    className="vp-checkbox__input"
-                    onChange={this.onChange}
-                    checked={this.state.checked}
+                    className="vp-control__input"
+                    onClick={this.onChange}
+                    checked={this.getCheckedState()}
                     id={id}
                     {...htmlAttributes}
                 />
-                <span className="vp-checkbox__span">{label}</span>
+                <span className="vp-control__span">{label}</span>
+                {!!error && <span className="vp-control__error"> {this.state.error} </span>}
             </label>
         );
     }
@@ -35,6 +60,7 @@ class Checkbox extends React.Component {
 
 Checkbox.propTypes = {
     onChange: PropTypes.func,
+    checked: PropTypes.bool,
     defaultChecked: PropTypes.bool,
     id: PropTypes.string.isRequired,
     validationMethod: PropTypes.func,
@@ -46,7 +72,7 @@ Checkbox.defaultProps = {
     defaultChecked: false,
     onChange: () => {},
     validationMethod: () => {},
-    className: () => {}
+    className: ''
 };
 
 export default Checkbox;
