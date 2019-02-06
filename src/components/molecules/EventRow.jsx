2c370cba3bedb4bb3ab8dc3963423b4977457a4e
Paige Vogenthaler <paige.vogenthaler@vividseats.com>
2019-02-06 11:55:37 -0600

    WEB-5743 finish up EventRow component

--- src/components/molecules/EventRow.jsx
+++ src/components/molecules/EventRow.jsx
@@ -8,73 +8,60 @@ import Link from '../atoms/Link';
 import Row from './Row';
 import SmallText from '../atoms/SmallText';
 
-const EventRow = ({ href, subtitle, title, dateRange = null, Image = null, production = null, type = 'default' }) => {
-    const { DATE_RANGE, IMAGE, PRODUCTION } = EventRow.TYPES;
-    const isType = staticType => staticType === type;
-    const hasButton = isType(PRODUCTION) || isType(DATE_RANGE);
+const EventRow = ({ href, subtitle, title, date = null, dateRange = null, thumbnail = null }) => {
+    const { dayOfWeek, month, dayOfMonth, time } = date || {};
+    const baseClassName = 'vp-event-row';
 
-    const { dayOfWeek, month, date, time } = production || {};
-    const eventRowClassName = 'vp-event-row';
-
-    /*
-    <a href="" class="vp-row--collapsed vp-event-row--has-button">
-        <div class="vp-col vp-event-row__date productionsDateCol">
-        <c:if test='${!production.dateTbd}'>
-        <p class="vp-type-small--muted--center">
-        THu
-        </p>
-        </c:if>
-        <p class="vp-type-body2--compressed--blk--caps--center">
-        AUG 12s
-        </p>
-        <p class="vp-type-small--muted--center--lowercase">
-        7:00pm
-        </p>
-        </div>
-        <div class="vp-col vp-event-row__info">
-        <p class="vp-type-body2--compressed--blk">
-        Production Name
-        </p>
-        <p class="vp-type-small--muted">
-        Venue Name
-        -
-        City, State
-        </p>
-        </div>
-        <div class="vp-col vp-event-row__button">
-        <button class="vp-button">Tickets</button>
-        </div>
-    </a>
-    */
+    const getColClassName = colType => `${baseClassName}__${colType}`;
+    const hasButton = !!date || !!dateRange;
 
     return (
-        <Link className={eventRowClassName} href={href}>
-            <Row className={`--collapsed ${eventRowClassName}${hasButton && '--has-button'}`}>
-                {!!Image && isType(IMAGE) && Image}
-                {!!production && isType(PRODUCTION) && (
-                    <Column className={`${eventRowClassName}__thumb`}>
-                        <SmallText alignment="center" state="muted">
-                            {!!dayOfWeek && dayOfWeek}
-                        </SmallText>
-                        <BodyText height="compressed" weight="black" capitalization="uppercase" alignment="center" importance={2}>
-                            {!!month && `${month} `}
-                            {!!date && date}
-                        </BodyText>
-                        <SmallText alignment="center" state="muted" capitalization="lowercase">
-                            {!!time && time}
-                        </SmallText>
+        <Link className={baseClassName} href={href} type="anchor">
+            <Row className={`--collapsed ${baseClassName}${hasButton && '--has-button'}`}>
+                {/* Thumbnail Image */}
+                {!!thumbnail && !!thumbnail.src && !!thumbnail.alt && (
+                    <Column className={getColClassName('thumbnail')}>
+                        <img src={thumbnail.src} alt={thumbnail.alt} />
                     </Column>
                 )}
-                <Column className={`${eventRowClassName}__info`}>
+                {/* Date */}
+                {!!date && (
+                    <Column className={getColClassName('date')}>
+                        {!!dayOfWeek && (
+                            <SmallText alignment="center" state="muted">
+                                {dayOfWeek}
+                            </SmallText>
+                        )}
+                        <BodyText height="compressed" weight="black" capitalization="uppercase" alignment="center" importance={2}>
+                            {!!month && `${month} `}
+                            {!!dayOfMonth && dayOfMonth}
+                        </BodyText>
+                        {!!time && (
+                            <SmallText alignment="center" state="muted" capitalization="lowercase">
+                                {time}
+                            </SmallText>
+                        )}
+                    </Column>
+                )}
+                {/* Event Info */}
+                <Column className={getColClassName('info')}>
                     <BodyText height="compressed" weight="black" importance={2}>
                         {title}
                     </BodyText>
                     <SmallText state="muted">{subtitle}</SmallText>
                 </Column>
-                {!!dateRange && isType(DATE_RANGE) && <Column className={`${eventRowClassName}`}>{dateRange}</Column>}
+                {/* Date Range */}
+                {!!dateRange && (
+                    <Column className={getColClassName('date-range')}>
+                        <SmallText alignment="right" state="muted">
+                            {dateRange}
+                        </SmallText>
+                    </Column>
+                )}
+                {/* Button */}
                 {hasButton && (
-                    <Column className={`${eventRowClassName}__button`}>
-                        <Button>{isType(PRODUCTION) ? 'Tickets' : 'View Event'}</Button>
+                    <Column className={getColClassName('button')}>
+                        <Button>{!!date ? 'Tickets' : 'View Event'}</Button>
                     </Column>
                 )}
             </Row>
@@ -82,25 +69,21 @@ const EventRow = ({ href, subtitle, title, dateRange = null, Image = null, produ
     );
 };
 
-EventRow.TYPES = {
-    DATE_RANGE: 'dateRange',
-    IMAGE: 'image',
-    PRODUCTION: 'production'
-};
-
 EventRow.propTypes = {
     href: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     subtitle: PropTypes.string.isRequired,
     dateRange: PropTypes.string,
-    Image: PropTypes.node,
-    production: PropTypes.shape({
+    thumbnail: {
+        src: PropTypes.string,
+        alt: PropTypes.string
+    },
+    date: PropTypes.shape({
         dayOfWeek: PropTypes.string,
+        dayOfMonth: PropTypes.string,
         month: PropTypes.string,
-        date: PropTypes.string,
         time: PropTypes.string
-    }),
-    type: PropTypes.oneOf(['default', 'dateRange', 'image', 'production'])
+    })
 };
 
 export default EventRow;
