8f5cf08f9b64f4c4bd287732e7b91312a9acd613
Eamon Almiladi <eamon.almiladi@vividseats.com>
2019-02-07 16:17:52 -0600

    [WEB-5735] refactor to single radio instance

--- src/components/atoms/Radio.jsx
+++ src/components/atoms/Radio.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const RadioOption = ({ isChecked, checked, className = '', error, label, children, ...attributes }) => (
+const Radio = ({ isChecked, checked, className = '', error, label, children, ...attributes }) => (
     <label className={`vp-control--radio ${className}`} aria-label="radio">
         <input hidden type="radio" className="vp-control__input" checked={checked || isChecked} {...attributes} />
         <span className="vp-control__span">{label}</span>
     </label>
 );
 
-export default RadioOption;
+export default Radio;
