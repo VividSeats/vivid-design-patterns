e0ea3cab6985c82d99223b3e09e1e267721f0a97
Eamon Almiladi <eamon.almiladi@vividseats.com>
2019-02-08 10:50:51 -0600

    [WEB-5735] radio unit tests

--- src/components/atoms/Radio.jsx
+++ src/components/atoms/Radio.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Radio = ({ id, isChecked, checked, className = '', label, children, ...attributes }) => (
+const Radio = ({ id, isChecked, checked, className = '', label, ...attributes }) => (
     <label className={`vp-control--radio ${className}`} aria-label="radio" htmlFor={id}>
         <input hidden type="radio" className="vp-control__input" id={id} checked={checked || isChecked} {...attributes} />
         <span className="vp-control__span">{label}</span>
     </label>
 );
+
+Radio.propTypes = {
+    checked: PropTypes.bool,
+    isChecked: PropTypes.bool,
+    id: PropTypes.string.isRequired,
+    label: PropTypes.string,
+    className: PropTypes.string
+};
 
 export default Radio;
---
+++ test/Radio/RadioSpec.js
@@ -0,0 +1,20 @@
+import { mount, shallow } from 'enzyme';
+import Radio from '../../src/components/atoms/Radio';
+import React from 'react';
+
+describe('<Radio />', () => {
+    it('renders a radio input', () => {
+        const wrapper = mount(<Radio name="fakeRadio" id="fakeRadio1" />);
+        expect(wrapper.find('label.vp-control--radio').exists()).toBe(true);
+    });
+
+    it('renders a radio input that is checked by default', () => {
+        const wrapper = shallow(<Radio name="fakeRadio" id="fakeRadio2" checked onChange={() => {}} />);
+        expect(wrapper.find('#fakeRadio2').prop('checked')).toBe(true);
+    });
+
+    it('renders a radio input with custom attributes', () => {
+        const wrapper = shallow(<Radio name="fakeRadio" id="fakeRadio2" className="hasFakeClass" />);
+        expect(wrapper.find('label.vp-control--radio').hasClass('hasFakeClass')).toBe(true);
+    });
+});
