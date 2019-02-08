c749d625fc9e3cdd5b8ee2d836e30ac3f6686209
Eamon Almiladi <eamon.almiladi@vividseats.com>
2019-02-08 13:30:39 -0600

    [WEB-5735] remove isChecked

--- src/components/atoms/Radio.jsx
+++ src/components/atoms/Radio.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Radio = ({ id, isChecked, checked, className, label, ...attributes }) => (
+const Radio = ({ id, checked, className, label, ...attributes }) => (
     <label className={`vp-control--radio ${className}`} aria-label="radio" htmlFor={id}>
-        <input hidden type="radio" className="vp-control__input" id={id} checked={checked || isChecked} {...attributes} />
+        <input hidden type="radio" className="vp-control__input" id={id} checked={checked} {...attributes} />
         <span className="vp-control__span">{label}</span>
     </label>
 );
