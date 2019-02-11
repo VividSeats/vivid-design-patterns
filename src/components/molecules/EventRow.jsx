b25342886e70ad3f3b00b8f0c51a88006e581ff9
Paige Vogenthaler <paige.vogenthaler@vividseats.com>
2019-02-11 13:29:30 -0600

    WEB-5743 clean up event row component, add unit tests

--- src/components/molecules/EventRow.jsx
+++ src/components/molecules/EventRow.jsx
@@ -10,26 +10,25 @@ import Row from './Row';
 import SmallText from '../atoms/SmallText';
 
 const EventRow = ({ href, subtitle, title, date = null, dateRange = null, thumbnail = null }) => {
-    const baseClassName = 'vp-event-row';
-    const getColClassName = colType => `${baseClassName}__${colType}`;
     const hasButton = !!date || !!dateRange;
-
-    const momentDate = moment(date);
+    const momentDate = moment(new Date(date));
+    const { getColClassName, BASE_CLASSNAME, COL_CLASSNAMES, BUTTON_TEXT } = EventRow;
+    const { BUTTON, DATE, DATE_RANGE, INFO, THUMBNAIL } = COL_CLASSNAMES;
 
     return (
-        <Link className={baseClassName} href={href} type="anchor">
-            <Row className={`--collapsed ${baseClassName}${hasButton && '--has-button'}`}>
+        <Link className={BASE_CLASSNAME} href={href} type="anchor">
+            <Row className={`--collapsed ${BASE_CLASSNAME}${hasButton && '--has-button'}`}>
                 {/* Thumbnail Image */}
                 {!!thumbnail && !!thumbnail.src && !!thumbnail.alt && (
-                    <Column className={getColClassName('thumbnail')}>
+                    <Column className={getColClassName(THUMBNAIL)}>
                         <img src={thumbnail.src} alt={thumbnail.alt} />
                     </Column>
                 )}
                 {/* Date */}
-                {!!momentDate && (
-                    <Column className={getColClassName('date')}>
+                {!!date && !!momentDate.isValid() && (
+                    <Column className={getColClassName(DATE)}>
                         <SmallText alignment="center" state="muted">
-                            {momentDate.format('dddd')}
+                            {momentDate.format('ddd')}
                         </SmallText>
                         <BodyText height="compressed" weight="black" capitalization="uppercase" alignment="center" importance={2}>
                             {momentDate.format('MMM D')}
@@ -40,7 +39,7 @@ const EventRow = ({ href, subtitle, title, date = null, dateRange = null, thumbn
                     </Column>
                 )}
                 {/* Event Info */}
-                <Column className={getColClassName('info')}>
+                <Column className={getColClassName(INFO)}>
                     <BodyText height="compressed" weight="black" importance={2}>
                         {title}
                     </BodyText>
@@ -48,7 +47,7 @@ const EventRow = ({ href, subtitle, title, date = null, dateRange = null, thumbn
                 </Column>
                 {/* Date Range */}
                 {!!dateRange && (
-                    <Column className={getColClassName('date-range')}>
+                    <Column className={getColClassName(DATE_RANGE)}>
                         <SmallText alignment="right" state="muted">
                             {dateRange}
                         </SmallText>
@@ -56,13 +55,29 @@ const EventRow = ({ href, subtitle, title, date = null, dateRange = null, thumbn
                 )}
                 {/* Button */}
                 {hasButton && (
-                    <Column className={getColClassName('button')}>
-                        <Button>{!!date ? 'Tickets' : 'View Event'}</Button>
+                    <Column className={getColClassName(BUTTON)}>
+                        <Button>{!!date ? BUTTON_TEXT.DATE : BUTTON_TEXT.DATE_RANGE}</Button>
                     </Column>
                 )}
             </Row>
         </Link>
     );
+};
+
+EventRow.BASE_CLASSNAME = 'vp-event-row';
+EventRow.getColClassName = colType => `${EventRow.BASE_CLASSNAME}__${colType}`;
+
+EventRow.COL_CLASSNAMES = {
+    BUTTON: 'button',
+    DATE: 'date',
+    DATE_RANGE: 'date-range',
+    INFO: 'info',
+    THUMBNAIL: 'thumbnail'
+};
+
+EventRow.BUTTON_TEXT = {
+    DATE: 'Tickets',
+    DATE_RANGE: 'View Event'
 };
 
 EventRow.propTypes = {
