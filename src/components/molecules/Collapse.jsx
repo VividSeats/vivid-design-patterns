0c68c5fe53e205b9626fa0a0a7dc15dc8ebfc70c
Yucheng Lin <yuchenglin3@gmail.com>
2019-02-27 16:57:45 -0600

    adding collapse component

---
+++ src/components/molecules/Collapse.jsx
@@ -0,0 +1,87 @@
+import React from 'react';
+import Subhead from '../atoms/Subhead';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+class Collapse extends React.Component {
+    state = {
+        open: this.props.open || this.props.initialOpen
+    };
+
+    isControlled() {
+        return typeof this.props.open !== 'undefined';
+    }
+    getOpenState() {
+        if (this.isControlled()) {
+            return this.props.open;
+        }
+
+        return this.state.open;
+    }
+
+    toggleCollapse = () => {
+        if (this.isControlled()) {
+            this.setState(({ open }) => ({ open: !open }));
+            this.props.onOpenChange(!this.getOpenState());
+        } else {
+            this.setState(({ open }) => ({ open: !open }));
+        }
+    };
+
+    render() {
+        const {
+            wrap,
+            mobileOnlyCollapse,
+            open,
+            initialOpen,
+            onOpenChange,
+            TitleElement,
+            children,
+            className,
+            title,
+            ...htmlAttributes
+        } = this.props;
+
+        const collapseClassName = classNames('vdp-collapse', {
+            '-wrap': wrap,
+            '--mobile': mobileOnlyCollapse
+        })
+            .split(' ')
+            .join('');
+
+        const dataState = this.getOpenState() ? 'opened' : 'closed';
+
+        return (
+            <div className={`${collapseClassName} ${className}`} {...htmlAttributes} data-state={dataState}>
+                {!!title && (
+                    <Subhead onClick={this.toggleCollapse} state={'muted'} className={`vdp-collapse__title`} {...htmlAttributes}>
+                        {title}
+                    </Subhead>
+                )}
+                <div className="vdp-collapse__content">{children}</div>
+            </div>
+        );
+    }
+}
+
+Collapse.propTypes = {
+    /** For controlled component **/
+    open: PropTypes.bool,
+    /** setting inital open for uncontrolled component **/
+    initialOpen: PropTypes.bool,
+    wrap: PropTypes.bool,
+    mobile: PropTypes.bool,
+    onOpenChange: PropTypes.func,
+    className: PropTypes.string,
+    title: PropTypes.string
+};
+
+Collapse.defaultProps = {
+    onOpenChange: () => {},
+    mobileOnlyCollapse: false,
+    wrap: false,
+    title: '',
+    className: ''
+};
+
+export default Collapse;
