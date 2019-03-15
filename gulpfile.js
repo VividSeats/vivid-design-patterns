f316a928d643d2354230085299a0ec774ef8f5df
Jeff Borders <jeff.borders@vividseats.com>
2019-03-15 13:14:06 -0500

    WEB-6070 reverting

--- gulpfile.js
+++ gulpfile.js
@@ -9,17 +9,15 @@ const isDev = process.env.NODE_ENV === 'dev';
 gulp.task('bundle_js', () => {
     const dist = 'dist';
 
-    return (
-        gulp
-            .src('src/components/**/*.{js,jsx}')
-            // .pipe(gulpif(isDev, changed(dist)))
-            .pipe(
-                rollUpEach(rollUpConfig, file => ({
-                    format: 'cjs'
-                }))
-            )
-            .pipe(gulp.dest(dist))
-    );
+    return gulp
+        .src('src/components/**/*.{js,jsx}')
+        .pipe(gulpif(isDev, changed(dist)))
+        .pipe(
+            rollUpEach(rollUpConfig, file => ({
+                format: 'cjs'
+            }))
+        )
+        .pipe(gulp.dest(dist));
 });
 
 gulp.task('watch_js', () => {
