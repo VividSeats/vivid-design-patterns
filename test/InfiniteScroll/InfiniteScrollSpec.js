9d7df58dc7f94f8b62fc8456d7556192f8e2ddc4
Yucheng Lin <yuchenglin3@gmail.com>
2019-03-18 15:04:04 -0500

    make tests for resilient

--- test/InfiniteScroll/InfiniteScrollSpec.js
+++ test/InfiniteScroll/InfiniteScrollSpec.js
@@ -5,28 +5,26 @@ import EventRow from '../../src/components/molecules/EventRow';
 
 describe('<InfiniteScroll />', () => {
     const mockLoadMore = jest.fn();
+    const numberOfChildren = 7;
+    const childHeight = 50;
     const InfiniteScrollComponent = (
         <InfiniteScroll style={{ height: '100px', overflowY: 'scroll' }} onLoadMore={mockLoadMore} isLoading={false}>
-            <EventRow style={{ height: '50px' }} subtitle="Event Location" href="!#" title="Event Name" />
-            <EventRow style={{ height: '50px' }} subtitle="Event Location" href="!#" title="Event Name" />
-            <EventRow style={{ height: '50px' }} subtitle="Event Location" href="!#" title="Event Name" />
-            <EventRow style={{ height: '50px' }} subtitle="Event Location" href="!#" title="Event Name" />
-            <EventRow style={{ height: '50px' }} subtitle="Event Location" href="!#" title="Event Name" />
-            <EventRow style={{ height: '50px' }} subtitle="Event Location" href="!#" title="Event Name" />
-            <EventRow style={{ height: '50px' }} subtitle="Event Location" href="!#" title="Event Name" />
+            {Array.from({ length: numberOfChildren }).map((value, index) => (
+                <EventRow style={{ height: `${childHeight}px` }} subtitle="Event Location" href="!#" title="Event Name" key={index} />
+            ))}
         </InfiniteScroll>
     );
 
     it('renders with its children', () => {
         const wrapper = mount(InfiniteScrollComponent);
         expect(wrapper.find('.vdp-infinite-scroll').exists()).toBe(true);
-        expect(wrapper.find('.vdp-infinite-scroll').children().length).toBe(7);
+        expect(wrapper.find('.vdp-infinite-scroll').children().length).toBe(numberOfChildren);
     });
 
     it('calls loadmore when you scroll to the bottom of the list', () => {
         const wrapper = mount(InfiniteScrollComponent);
         const rootDomNode = wrapper.getDOMNode();
-        rootDomNode.scrollTop = 350;
+        rootDomNode.scrollTop = numberOfChildren * childHeight;
         wrapper.simulate('scroll', {
             target: rootDomNode
         });
