1f602c1d36265c38dd475a81f4f598f086e7bc1f
Yucheng Lin <yuchenglin3@gmail.com>
2019-04-11 14:13:05 -0500

    remove null default values

--- src/components/atoms/DateColumn.jsx
+++ src/components/atoms/DateColumn.jsx
@@ -5,7 +5,7 @@ import SmallText from './SmallText';
 import BodyText from './BodyText';
 import Badge from './Badge';
 
-export default function DateColumn({ date = null, isTimeTbd = false }) {
+export default function DateColumn({ date, isTimeTbd }) {
     const momentDate = moment(date);
     const eventRowClass = 'vdp-event-row__col--date';
 
