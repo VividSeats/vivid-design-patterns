aaa16d2c5c0a5a649082b4657bf57e66b3c7f68d
David Metzler <david.metzler@vividseats.com>
2019-04-15 12:33:48 -0500

    Added tests and refactored existing tests

--- test/ExpandableContent/ExpandableContentSpec.js
+++ test/ExpandableContent/ExpandableContentSpec.js
@@ -4,18 +4,37 @@ import ExpandableContent from '../../src/components/molecules/ExpandableContent'
 
 describe('<ExpandableContent />', () => {
     it('renders an expandable container', () => {
-        const wrapper = mount(<ExpandableContent>Four score and seven years ago...</ExpandableContent>);
+        const wrapper = mount(
+            <ExpandableContent maxHeight="30rem" buttonText="Show More Stuff">
+                Four score and seven years ago...
+            </ExpandableContent>
+        );
+        wrapper.setState({ isCollapsed: true });
+
         const container = wrapper.find('.vdp-expandable-content');
         expect(container.exists()).toBe(true);
         expect(container.hasClass('__collapsed')).toBe(true);
+        expect(container.prop('style')).toEqual({ maxHeight: '30rem' });
+
+        const button = wrapper.find('.vdp-button');
+        expect(button.exists()).toBe(true);
+        expect(button.text()).toEqual('Show More Stuff');
     });
 
     it('expands container on button click', () => {
         const wrapper = mount(<ExpandableContent>Four score and seven years ago...</ExpandableContent>);
+        wrapper.setState({ isCollapsed: true });
+
+        let container = wrapper.find('.vdp-expandable-content');
+        expect(container.exists()).toBe(true);
+        expect(container.hasClass('__collapsed')).toBe(true);
+
         const button = wrapper.find('.vdp-button');
         button.simulate('click');
-        const container = wrapper.find('.vdp-expandable-content');
+
+        container = wrapper.find('.vdp-expandable-content');
         expect(container.exists()).toBe(true);
         expect(container.hasClass('__collapsed')).toBe(false);
+        expect(container.prop('style')).toEqual({ maxHeight: '999rem' });
     });
 });
