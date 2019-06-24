410bcfb0fb709f4d9fbe94929779553d35b4b614
Charlie Moog <charlie.moog@vividseats.com>
2019-06-24 16:31:49 -0500

    moves dirty state logic to proper lifecycle method

--- src/components/molecules/TextField.jsx
+++ src/components/molecules/TextField.jsx
@@ -46,11 +46,17 @@ class TextField extends React.Component {
     };
 
     componentDidUpdate(prevProps) {
-        const { error } = this.props;
-        if (error !== prevProps.error) {
+        const { error, value } = this.props;
+        const { dirty } = this.state;
+        const isDirty = !!value;
+
+        if (!dirty && prevProps.value !== value) {
             this.setState({
-                error
+                dirty: isDirty
             });
+        }
+        if (error !== prevProps.error) {
+            this.setState({ error });
         }
     }
 
@@ -107,8 +113,6 @@ class TextField extends React.Component {
             filled: active || !!value,
             success: !error && dirty && !noValidate
         });
-
-        if (!dirty && !!value) this.setState({ dirty: true });
 
         const inputProps = {
             id,
