dbacf4c66ace56557e0c6dc2a81d042dd879ec10
Yucheng Lin <yucheng.lin@vividseats.com>
2019-06-27 19:15:58 -0500

    update vdp components with structured data

--- src/components/molecules/EventRow.jsx
+++ src/components/molecules/EventRow.jsx
@@ -11,6 +11,7 @@ const EventRow = ({
     href,
     subtitle,
     title,
+    venue = {},
     date = null,
     dateRange = null,
     thumbnail = null,
@@ -20,9 +21,19 @@ const EventRow = ({
 }) => {
     const { getColClassName, BASE_CLASSNAME, COL_CLASSNAMES, BUTTON_TEXT } = EventRow;
     const { BUTTON, DATE_RANGE, INFO, THUMBNAIL } = COL_CLASSNAMES;
+    const { regionCode, countryCode, city, name: venueName } = venue;
+    const regionCodeString = regionCode ? `, ${regionCode}` : '';
+    const countryCodeString = countryCode !== 'US' ? `, ${countryCode}` : '';
 
     return (
-        <Link className={BASE_CLASSNAME} href={href} type="anchor" {...htmlAttributes}>
+        <Link
+            className={BASE_CLASSNAME}
+            href={href}
+            type="anchor"
+            itemScope
+            itemType="http://schema.org/Event"
+            role="row"
+            {...htmlAttributes}>
             {/* Thumbnail Image */}
             {!!thumbnail && !!thumbnail.src && !!thumbnail.alt && (
                 <div className={getColClassName(THUMBNAIL)}>
@@ -33,10 +44,19 @@ const EventRow = ({
             <DateColumn date={date} isTimeTbd={isTimeTbd} />
             {/* Event Info */}
             <div className={getColClassName(INFO)}>
-                <BodyText height="compressed" weight="black" importance={2}>
+                <BodyText height="compressed" weight="black" importance={2} itemProp="name">
                     {title}
                 </BodyText>
-                <SmallText state="muted">{subtitle}</SmallText>
+                {!!Object.keys(venue).length ? (
+                    <SmallText state="muted" itemProp="location" itemScope itemType="http://schema.org/Place">
+                        <span itemProp="name">{venueName}</span>&nbsp;–&nbsp;
+                        <span itemProp="address" itemScope itemType="http://schema.org/PostalAddress">
+                            {`${city}${regionCodeString}${countryCodeString}`}
+                        </span>
+                    </SmallText>
+                ) : (
+                    <SmallText state="muted">subtitle</SmallText>
+                )}
             </div>
             {/* Date Range */}
             {!!dateRange && (
@@ -73,8 +93,14 @@ EventRow.BUTTON_TEXT = {
 
 EventRow.propTypes = {
     href: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
-    subtitle: PropTypes.string.isRequired,
+    venue: PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        city: PropTypes.string.isRequired,
+        regionCode: PropTypes.string.isRequired,
+        countryCode: PropTypes.string
+    }),
+    title: PropTypes.node.isRequired,
+    subtitle: PropTypes.node,
     dateRange: PropTypes.string,
     date: PropTypes.oneOfType([PropTypes.string, PropTypes.number, PropTypes.instanceOf(Date)]),
     thumbnail: PropTypes.shape({
