7c8bc60ecaf8cdce5e38857d373823399c91a934
Charlie Moog <charlie.moog@vividseats.com>
2019-08-09 16:42:25 -0500

    combines notification and toast components

--- src/components/molecules/Notification.jsx
+++ src/components/molecules/Notification.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Toast from '../atoms/Toast';
+import Icon from '../atoms/Icon';
+import Button from '../atoms/Button';
 
-const Notification = ({ isOpen, children, className = '', animateOpacity, ...props }) => {
+const Notification = ({ isOpen, children, className = '', type, onClickClose = () => {}, ...props }) => {
     return (
-        <Toast {...{ isOpen, animateOpacity }}>
-            <div className={`vdp-notification ${className}`} {...props}>
-                {children}
-            </div>
-        </Toast>
+        <div className={`vdp-notification vdp-${type} ${isOpen ? 'open' : ''} ${className}`} {...props}>
+            {type === 'toast' && (
+                <Button importance="text" muted className="close-btn" onClick={onClickClose}>
+                    <Icon type="close" />
+                </Button>
+            )}
+            {children}
+        </div>
     );
 };
 
@@ -16,7 +20,8 @@ Notification.propTypes = {
     isOpen: PropTypes.bool.isRequired,
     children: PropTypes.node,
     className: PropTypes.string,
-    animateOpacity: PropTypes.bool
+    type: PropTypes.oneOf(['toast']).isRequired,
+    onClickClose: PropTypes.func
 };
 
 export default Notification;
