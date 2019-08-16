506cba22ae5a179568ca5176a657e90ad3b25dda
Sam Gibson <sam.gibson@vividseats.com>
2019-08-16 16:03:06 -0500

    DOT-2 format promo banner; add test file for promo banner

--- src/components/molecules/PromoBanner.jsx
+++ src/components/molecules/PromoBanner.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Icon from '../atoms/Icon';
 import TinyText from '../atoms/TinyText';
 import Subhead from '../atoms/Subhead';
 import Row from './Row';
@@ -33,7 +32,7 @@ class PromoBanner extends React.Component {
         const { offerType, offerHeadline, offerSubheadline, offerDetails, legalText } = this.props;
         return (
             <div className="vdp-promo-banner">
-                <Row>
+                <Row className="bg-slate">
                     <Subhead className="vdp-type-subhead--gold">{offerType}</Subhead>
                     <Subhead className="vdp-type-subhead--inverted">{offerHeadline}</Subhead>
                     <Subhead className="vdp-type-subhead--inverted">{offerSubheadline}</Subhead>
