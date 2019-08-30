30036ce06a4291626f1a9a613977e6e462b8e148
Yucheng Lin <yucheng.lin@vividseats.com>
2019-08-30 14:56:08 -0500

    change prop types for x button to look at onCloseClick

--- src/components/molecules/Notification.jsx
+++ src/components/molecules/Notification.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Icon from '../atoms/Icon';
 
-const Notification = ({ isOpen, children, className = '', type, onClickClose, ...props }) => {
+const Notification = ({ isOpen, children, className = '', onClickClose, ...props }) => {
     return (
         <div
             className={`vdp-notification vdp-notification--${type} ${isOpen ? 'vdp-notification--toast--open' : ''} ${className}`}
@@ -17,7 +17,6 @@ Notification.propTypes = {
     isOpen: PropTypes.bool.isRequired,
     children: PropTypes.node,
     className: PropTypes.string,
-    type: PropTypes.oneOf(['toast']).isRequired,
     /** Called when user clicks on the 'X' button in the Notification. If a click handler is not passed, the 'X' will not be displayed */
     onClickClose: PropTypes.func
 };
