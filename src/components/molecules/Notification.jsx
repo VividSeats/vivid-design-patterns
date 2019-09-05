854523b408ff5867c5f3ca8e52eba8a80030cc4c
Charlie Moog <charlie.moog@vividseats.com>
2019-09-05 10:41:52 -0500

    adds js animations

--- src/components/molecules/Notification.jsx
+++ src/components/molecules/Notification.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Backdrop from '../atoms/Backdrop';
 import Icon from '../atoms/Icon';
 
-const Notification = ({ isOpen, children, className = '', type, onClickClose = () => {}, ...props }) => {
+const Notification = ({ isOpen, children, className = '', type, onClickClose = () => {}, hasBackdrop = false, ...props }) => {
     return (
         <div
             className={`vdp-notification vdp-notification--${type} ${isOpen ? 'vdp-notification--toast--open' : ''} ${className}`}
             {...props}>
             {type === 'toast' && <Icon type="close" className="vdp-notification__dismiss" onClick={onClickClose} />}
+            {hasBackdrop && <Backdrop isOpen={isOpen} />}
             {children}
         </div>
     );
@@ -18,7 +20,8 @@ Notification.propTypes = {
     children: PropTypes.node,
     className: PropTypes.string,
     type: PropTypes.oneOf(['toast']).isRequired,
-    onClickClose: PropTypes.func
+    onClickClose: PropTypes.func,
+    hasBackdrop: PropTypes.bool
 };
 
 export default Notification;
