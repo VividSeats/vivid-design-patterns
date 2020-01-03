2b7409d8311c27bc307a3261f45e36b549979da8
Yucheng Lin <yucheng.lin@vividseats.com>
2020-01-03 14:53:50 -0800

    Update grid components with more documentation and responsive flex props (#149)
    * update grid components with more documentation and props
    
    * Make utilities important
    
    * [gridComponents] add auto-hyphens
    
    * [gridComponents] update package
    
    * add flex-wrap
    
    * update prop types
    
    * add responsive props
    
    * add flexWrap default prop
    
    * add flexWrap prop types
    
    * add flexWrap prop types
    
    * make flexDirection prop array or value
    
    * add documentation
    
    * fix typo
    
    Co-authored-by: Matt Gorman <matt.gorman@vividseats.com>

--- src/components/molecules/Row.jsx
+++ src/components/molecules/Row.jsx
@@ -1,15 +1,53 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 
-const Row = ({ className = '', children, ...htmlAttributes }) => (
-    <div className={`vdp-row${!!className.length ? ` ${className}` : ''}`} {...htmlAttributes}>
-        {children}
-    </div>
-);
+const getFlexDirectionClass = (flexDirection, device) => {
+    if (flexDirection === 'column') {
+        return `--col-${device}`;
+    }
+    if (flexDirection === 'column-reverse') {
+        return `--col-reverse-${device}`;
+    }
+    if (flexDirection === 'row-reverse') {
+        return `--reverse-${device}`;
+    }
+};
+
+const Row = ({ className = '', children, justify, align, flexDirection = [], flexWrap = [], ...htmlAttributes }) => {
+    const rowClassNames = classNames('vdp-row', {
+        [className]: className,
+        [`--justify-${justify}`]: justify,
+        [`--align-${align}`]: align,
+        '--column': flexDirection === 'column',
+        [getFlexDirectionClass(flexDirection[0], 'mobile')]: Array.isArray(flexDirection) && flexDirection[0],
+        [getFlexDirectionClass(flexDirection[1], 'tablet')]: Array.isArray(flexDirection) && flexDirection[1],
+        [getFlexDirectionClass(flexDirection[2], 'desktop')]: Array.isArray(flexDirection) && flexDirection[2],
+        [`--${flexWrap}-all`]: flexWrap && !Array.isArray(flexWrap),
+        [`--${flexWrap[0]}-mobile`]: Array.isArray(flexWrap) && flexWrap[0],
+        [`--${flexWrap[1]}-tablet`]: Array.isArray(flexWrap) && flexWrap[1],
+        [`--${flexWrap[2]}-desktop`]: Array.isArray(flexWrap) && flexWrap[2]
+    });
+
+    return (
+        <div className={rowClassNames} {...htmlAttributes}>
+            {children}
+        </div>
+    );
+};
 
 Row.propTypes = {
     className: PropTypes.string,
-    children: PropTypes.node
+    children: PropTypes.node,
+    justify: PropTypes.oneOf(['start', 'center', 'end', 'around', 'between']),
+    align: PropTypes.oneOf(['top', 'bottom', 'center']),
+    /** sets flex-wrap css property. Use array for responsive breakpoint where the first item correspond to the smallest breakpoint. If you pass a value instead, will apply to all breakpoint  */
+    flexDirection: PropTypes.oneOf([
+        PropTypes.string,
+        PropTypes.arrayOf(PropTypes.oneOf(['row', 'column', 'column-reverse', 'row-reverse']))
+    ]),
+    /** sets flex-wrap css property. Use array for responsive breakpoint where the first item correspond to the smallest breakpoint. If you pass a value instead, will apply to all breakpoint  */
+    flexWrap: PropTypes.oneOf([PropTypes.string, PropTypes.arrayOf(PropTypes.oneOf(['wrap', 'nowrap']))])
 };
 
 export default Row;
