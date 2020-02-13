ddceaf8d6c29a362704b47524acef3810ca17d6f
mgorman87 <matt.gorman@vividseats.com>
2020-02-13 10:23:07 -0600

    DSYS-73 fix trigger tests

--- test/Trigger/TriggerSpec.js
+++ test/Trigger/TriggerSpec.js
@@ -1,27 +1,28 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import Trigger from '../../src/components/atoms/Trigger';
 
 describe('<Trigger />', () => {
     const mockOnClick = jest.fn();
 
     it('renders a trigger', () => {
-        const wrapper = shallow(<Trigger icon="refresh" onClick={mockOnClick} />);
-        expect(wrapper.find('button.vdp-trigger i.vdp-icon-refresh').exists()).toBe(true);
+        const wrapper = mount(<Trigger icon="refresh" onClick={mockOnClick} />);
+        expect(wrapper.find('button.vdp-trigger').exists()).toBe(true);
+        expect(wrapper.find('.vdp-icon-refresh').exists()).toBe(true);
     });
 
     it('renders a dark trigger', () => {
-        const wrapper = shallow(<Trigger icon="refresh" dark onClick={mockOnClick} />);
+        const wrapper = mount(<Trigger icon="refresh" dark onClick={mockOnClick} />);
         expect(wrapper.find('button.vdp-trigger--dark').exists()).toBe(true);
     });
 
     it('renders a positioned trigger', () => {
-        const wrapper = shallow(<Trigger icon="refresh" position="tl" onClick={mockOnClick} />);
+        const wrapper = mount(<Trigger icon="refresh" position="tl" onClick={mockOnClick} />);
         expect(wrapper.find('button.vdp-trigger--tl').exists()).toBe(true);
     });
 
     it('handles onClick event', () => {
-        const wrapper = shallow(<Trigger icon="refresh" onClick={mockOnClick} />);
+        const wrapper = mount(<Trigger icon="refresh" onClick={mockOnClick} />);
         wrapper.find('button.vdp-trigger').simulate('click');
         expect(mockOnClick).toHaveBeenCalledTimes(1);
     });
