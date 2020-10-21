124be4df9ef7a4222bc01b65725a8bba33046a1d
Roe Greene <roe.greene@vividseats.com>
2020-10-21 16:36:42 -0500

    add support for next links

--- src/components/atoms/Link.jsx
+++ src/components/atoms/Link.jsx
@@ -1,54 +1,60 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 import PropTypes from 'prop-types';
 import { getTypeClassNames, TYPOGRAPHY_PROP_TYPES } from '../../utils/typographyUtils';
 
-const Link = ({
-    children,
-    type = 'link',
-    className,
-    href,
-    onClick,
-    weight,
-    height,
-    state,
-    alignment,
-    capitalization,
-    list,
-    truncate,
-    opaque,
-    ...attributes
-}) => {
-    Link.propTypes = {
-        children: PropTypes.node,
-        className: PropTypes.string,
-        href: PropTypes.string.isRequired,
-        type: PropTypes.oneOf(['link', 'anchor']),
-        onClick: PropTypes.func,
-        ...TYPOGRAPHY_PROP_TYPES
-    };
+const Link = forwardRef(
+    (
+        {
+            children,
+            type = 'link',
+            className,
+            href,
+            onClick,
+            weight,
+            height,
+            state,
+            alignment,
+            capitalization,
+            list,
+            truncate,
+            opaque,
+            ...attributes
+        },
+        ref
+    ) => {
+        Link.propTypes = {
+            children: PropTypes.node,
+            className: PropTypes.string,
+            href: PropTypes.string,
+            type: PropTypes.oneOf(['link', 'anchor']),
+            onClick: PropTypes.func,
+            ...TYPOGRAPHY_PROP_TYPES
+        };
 
-    const baseClassName = type === 'anchor' ? 'vdp-anchor' : 'vdp-type-link';
-    const linkClassNames = getTypeClassNames(baseClassName, {
-        weight,
-        height,
-        state,
-        alignment,
-        capitalization,
-        truncate,
-        list,
-        opaque,
-        className
-    });
+        const baseClassName = type === 'anchor' ? 'vdp-anchor' : 'vdp-type-link';
+        const linkClassNames = getTypeClassNames(baseClassName, {
+            weight,
+            height,
+            state,
+            alignment,
+            capitalization,
+            truncate,
+            list,
+            opaque,
+            className
+        });
 
-    const props = {
-        href,
-        onClick,
-        role: 'link',
-        className: linkClassNames,
-        ...attributes
-    };
+        const props = {
+            href,
+            onClick,
+            role: 'link',
+            className: linkClassNames,
+            ref,
+            ...attributes
+        };
 
-    return <a {...props}>{children}</a>;
-};
+        return <a {...props}>{children}</a>;
+    }
+);
 
 export default Link;
